301c59d7bc4c13b360cb52ee2946c152d365dedf
Wilson Padilla <wdavidw26@gmail.com>
2023-06-18 15:14:01 -0500

    :tada: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,11 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "linebreak-style": 0,
+    "eol-last": 0,
+    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+    // quotes: [1, "single", { avoidEscape: true }],
   },
   overrides: [
     {
