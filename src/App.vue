23775af6e202b1f979ad564c2065d079c4cdcf99
Wilson Padilla <wdavidw26@gmail.com>
2023-06-18 15:14:17 -0500

    :lipstick: import styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <Title />
+    <router-view />
+  </div>
 </template>
+
+<script>
+import Title from "@/components/Title.vue";
+export default {
+  components: {
+    Title,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-color: #ebf2f5;
+  font-family: Roboto, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
