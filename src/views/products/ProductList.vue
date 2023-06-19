cc35aceb589c9eab81ff96124b309fe9e8d6f0b2
Wilson Padilla <wdavidw26@gmail.com>
2023-06-18 19:45:09 -0500

    :sparkles: layout product list

---
+++ src/views/products/ProductList.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <div class="list__title">
+      <span>Store</span>
+    </div>
+    <div class="list__divider"></div>
+    <div class="products-list">
+      <div
+        v-for="(product, index) in products"
+        :key="index"
+        class="products-list__item"
+        v-bind:class="product.id == productSelected.id ? 'item-select' : ''"
+        @click="selectProduct(index)"
+      >
+        <img :src="product.image" />
+        <div
+          v-if="product.hasOwnProperty('count') && product.count > 0"
+          class="item-count"
+        >
+          <span>{{ product.count }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from "vuex";
+const products = createNamespacedHelpers("products");
+export default {
+  name: "ProductList",
+  mounted() {
+    this.getProducts();
+  },
+
+  computed: {
+    ...products.mapGetters(["products", "cart", "total", "productSelected"]),
+  },
+
+  methods: {
+    ...products.mapActions(["getProducts", "setProduct"]),
+
+    selectProduct(index) {
+      this.setProduct(index);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.list {
+  &__title {
+    text-align: start;
+    font-size: 1.5rem;
+    color: $color-primary;
+    font-family: "RobotoBold";
+  }
+
+  &__divider {
+    background-color: $color-secondary-variation;
+    height: 5px;
+    width: 100%;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+}
+.products-list {
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 1rem;
+  overflow: auto;
+  &__item {
+    border: 3.5px solid grey;
+    height: 250px;
+    position: relative;
+    & img {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+      top: 0;
+      left: 0;
+    }
+  }
+}
+
+.item-count {
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  background-color: $color-primary;
+  text-align: center;
+  position: absolute;
+  top: 3px;
+  left: 3px;
+  & span {
+    line-height: 40px;
+    vertical-align: middle;
+    color: white;
+  }
+}
+
+.item-select {
+  border: 3.5px solid $color-primary;
+  box-shadow: 10px 10px 46px -17px $color-primary;
+}
+</style>
