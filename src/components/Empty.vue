aaf4b044aab038ca47812b1bba8782038c0079a5
Wilson Padilla <wdavidw26@gmail.com>
2023-06-18 19:44:09 -0500

    :sparkles: layout empty component

---
+++ src/components/Empty.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="empty-container">
+    <div class="divider"></div>
+    <span>Please choose a product on the left</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmptyVue",
+};
+</script>
+
+<style lang="scss" scoped>
+.empty-container {
+  width: 100%;
+  text-align: left;
+  color: $text-secondary;
+  font-family: "RobotoBold";
+}
+.divider {
+  background-color: $color-secondary-variation;
+  height: 5px;
+  width: 100%;
+  margin-top: 45px;
+  margin-bottom: 10px;
+}
+</style>
